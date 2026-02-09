9f491190e5ca776dc013d762cb9f2b0ef97977cb
TheVelvetShadow <matt@thevelvetshadow.com>
2026-02-09 20:55:24 +0000

    Background Colour - SFX & Music

--- godot-2d-tutorial/main.gd
+++ godot-2d-tutorial/main.gd
@@ -18,8 +18,10 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()	
 	$HUD.show_game_over()
-
-
+	$Music.stop()
+	$DeathSound.play()
+	
+	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
@@ -27,6 +29,8 @@ func new_game():
 	
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	
+	$Music.play()
 	
 func _on_score_timer_timeout() -> void:
 	score += 1
