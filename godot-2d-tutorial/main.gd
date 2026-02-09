2328b6e0c21aec146fbed1a3bbcfd9f05f4b6bed
TheVelvetShadow <matt@thevelvetshadow.com>
2026-02-09 20:18:30 +0000

    Final Game

--- godot-2d-tutorial/main.gd
+++ godot-2d-tutorial/main.gd
@@ -6,7 +6,7 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_game()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,18 +17,25 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()	
+	$HUD.show_game_over()
+
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()	
-
+	
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	
 func _on_score_timer_timeout() -> void:
 	score += 1
+
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
 	$ScoreTimer.start()
+
 
 func _on_mob_timer_timeout() -> void:
 	# Create a new instance of the Mob scene.
